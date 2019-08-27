59382aa7469a3e674123616d8d4cb04798e3fe8c
Glenn A Doherty <gadz@earthlink.net>
2019-08-27 22:00:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,8 @@ def load_library (file_path)
   return new_translator
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file_path, west_emot)
+  
 end
 
 def get_english_meaning
